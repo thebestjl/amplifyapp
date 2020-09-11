d3d5e1966b1e5feeef1e231d7330508d101e1f98
John Lee <johndlee90@gmail.com>
2020-09-10 19:11:15 -0500

    added storage

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -3,11 +3,17 @@
 
 const awsmobile = {
     "aws_project_region": "us-east-2",
-    "aws_cognito_identity_pool_id": "us-east-2:a4844540-93a9-4490-8902-36f93c039cd8",
+    "aws_cognito_identity_pool_id": "us-east-2:769c741a-c367-4faa-894f-cf00d59d4de2",
     "aws_cognito_region": "us-east-2",
-    "aws_user_pools_id": "us-east-2_NtEE3luJn",
-    "aws_user_pools_web_client_id": "4h6nq8faqudg1na9c4t06qgg3",
-    "oauth": {}
+    "aws_user_pools_id": "us-east-2_nwCUvlRjr",
+    "aws_user_pools_web_client_id": "6366b1nafc05ndvlv4hvvf44as",
+    "oauth": {},
+    "aws_appsync_graphqlEndpoint": "https://cbek4qg2jzdlnmfyjc6trfgljm.appsync-api.us-east-2.amazonaws.com/graphql",
+    "aws_appsync_region": "us-east-2",
+    "aws_appsync_authenticationType": "API_KEY",
+    "aws_appsync_apiKey": "da2-ghgcsjhasnb5pdgftmkvvar4iu",
+    "aws_user_files_s3_bucket": "amplifyapp90b3d822cc7f4a60a91462c271315d1d150828-dev",
+    "aws_user_files_s3_bucket_region": "us-east-2"
 };
 
 
